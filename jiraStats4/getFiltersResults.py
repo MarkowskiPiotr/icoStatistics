89710ade6b9db797a0a427c00b38b8af9895d207
PiotrM <piotrm@MacBook-Pro.local>
2021-05-05 09:44:39 +0200

    initial version without config

---
+++ jiraStats4/getFiltersResults.py
@@ -0,0 +1,93 @@
+from jira import JIRA
+import configparser
+
+config = configparser.ConfigParser()
+config.read('configuration.ini')
+
+
+jiraAddress = config['JIRA']['jiraAddress']
+userName = config['JIRA']['userName']
+userAPIToken = config['JIRA']['userAPIToken']
+
+
+
+
+options = {
+    'server': jiraAddress
+}
+
+jira = JIRA(options, basic_auth=(userName, userAPIToken))
+
+ticketClosedStatuses = "Done, Closed, \"Won't Do\",Duplicate"
+ticketOpenStatuses = "\"To Do\", Backlog, \"In Progress\", \"TODO QA\", \"Waiting for Code Review\""
+# projectsStatistics = {}
+
+
+def generateStatistics(filterProject, ticketStatuses, ticketPriority, ticketDate, dayBefore):
+    projectsStatistics = {'newBugs': {}, 'notClosedBugs': {}, 'notClosedBugsPriority': {},
+                                         'notClosedBugsStatus': {},
+                                         'ticketsInQA': {}, 'ticketsInQAPriority': {}}
+    projectsStatistics['newBugs'] = getNewBugs(filterProject, ticketDate, dayBefore)
+    projectsStatistics['notClosedBugs'] = getAllBugs(filterProject, ticketClosedStatuses, ticketDate)
+    projectsStatistics['notClosedBugsPriority']=getBugsByPriority(filterProject, ticketPriority, ticketDate)
+    projectsStatistics['notClosedBugsStatus']=getBugsByStatus(filterProject, ticketStatuses,ticketDate)
+    projectsStatistics['ticketsInQA']=getTicketsByPriority(filterProject, ticketDate)
+    projectsStatistics['ticketsInQAPriority']=getTicketsByStatus(filterProject, ticketPriority, ticketDate)
+
+
+
+
+    return projectsStatistics
+
+def getNewBugs(filterProject, ticketDate, dayBefore):
+    jiraFilter = "project in (" + filterProject + ") AND type = Bug AND createdDate >= '"\
+                 + dayBefore + "' AND createdDate <= '" + ticketDate + "'"
+    jiraResult = jira.search_issues(jiraFilter, maxResults=1)
+    # print(jiraResult)
+    return jiraResult.total
+
+def getAllBugs(filterProject, ticketClosedStatuses, ticketDate):
+    jiraFilter = "project in (" + filterProject + ") AND type = Bug AND status was not in (" \
+                     + ticketClosedStatuses + ") on ('" + ticketDate + " 06:00')"
+    jiraResult = jira.search_issues(jiraFilter, maxResults=1)
+    return jiraResult.total
+
+def getBugsByPriority(filterProject, ticketPriority,ticketDate):
+    projectsStatistics={}
+    for filterPriority in ticketPriority:
+        jiraFilter = "project in (" + filterProject + ") AND type in (Bug) AND status was not in (" \
+                 + ticketClosedStatuses + ") on ('" + ticketDate + " 06:00') " + "AND priority = '" + filterPriority + "'"
+        jiraResult = jira.search_issues(jiraFilter, maxResults=1)
+        projectsStatistics[filterPriority] = jiraResult.total
+    return projectsStatistics
+
+def getBugsByStatus(filterProject, ticketStatuses,ticketDate):
+    projectsStatistics = {}
+    for filterStatus in ticketStatuses:
+        jiraFilter = "project in (" + filterProject + ") AND type in (Bug) AND status was '" + filterStatus + "'" \
+                     + " on ('" + ticketDate + " 06:00')"
+        jiraResult = jira.search_issues(jiraFilter, maxResults=1)
+        projectsStatistics[filterStatus] = jiraResult.total
+    return projectsStatistics
+
+def getTicketsByPriority(filterProject, ticketDate):
+    projectsStatistics = {}
+    for filterStatus in ("Ready for testing", "In testing"):
+        jiraFilter = "project in (" + filterProject + ") AND type !=Bug AND status was '" + filterStatus + "' on ('" \
+                     + ticketDate + " 06:00')"
+        jiraResult = jira.search_issues(jiraFilter, maxResults=1)
+        projectsStatistics[filterStatus] = jiraResult.total
+    return projectsStatistics
+
+def getTicketsByStatus(filterProject, ticketPriority, ticketDate):
+    projectsStatistics = {}
+    for filterStatus in ("Ready for testing", "In testing"):
+        projectsStatistics[filterStatus]={}
+        for filterPriority in ticketPriority:
+            jiraFilter = "project in (" + filterProject \
+                         + ") AND type !=Bug AND status was in ('" + filterStatus + "') on ('" + ticketDate + " 06:00') AND priority = '" \
+                         + filterPriority + "'"
+            jiraResult = jira.search_issues(jiraFilter, maxResults=1)
+            projectsStatistics[filterStatus][filterPriority] = jiraResult.total
+    return projectsStatistics
+
---
+++ jiraStats4/getStatistics.py
@@ -0,0 +1,72 @@
+import sendEmail
+import json
+import getFiltersResults
+from datetime import date, timedelta, datetime
+import os
+import getopt
+import sys
+import configparser
+
+config = configparser.ConfigParser()
+config.read('configuration.ini')
+
+defaultNoOfDays = int(config['LOGS']['defaultNoOfDays'])
+defaultLogsLocation = config['LOGS']['defaultLogsLocation']
+
+
+
+argv = sys.argv[1:]
+
+if len(argv) >0:
+    try:
+        opts, args = getopt.getopt(argv, 'd:')
+        if len(opts) > 1:
+            print('usage: getStatistics.py -d <number_of_days>')
+        else:
+            ticketDatesRange = int(opts[0][1])
+    except getopt.GetoptError:
+        print('usage: getStatistics.py -d <number_of_days>')
+        sys.exit(2)
+else:
+    ticketDatesRange = defaultNoOfDays
+
+#ticketProjects = ["'MLGTINT', 'MLGTFON'"]
+#ticketProjects = ['CAMINT', 'CORINT', 'IGW4000', "'MLGTINT', 'MLGTFON'", "'MLLTINT', 'MLLTFON'", 'I520XTQA', 'SPYDINT', 'I488271INT' ]
+ticketProjects = ['CORINT', 'IGW4000', "'MLGTINT', 'MLGTFON'", "'MLLTINT', 'MLLTFON'", "'I520XTQA', 'I520XTW'", 'SPYDINT', 'I488271INT' ]
+ticketStatuses = ['To do', 'Ready for Work', 'In Progress', 'On Hold', 'Ready for testing', 'In Testing', 'Feedback']
+ticketPriority = ['Blocker', 'High', 'Normal', 'Low', 'Not set']
+
+
+projectsStatistics = {}
+todayDate = date.today()
+
+currentTime = datetime.now().strftime("%H_%M")
+
+firstDate = todayDate - timedelta(days=ticketDatesRange-1)
+
+filterDates=[]
+
+for datesIterator in range(ticketDatesRange):
+        filterDates.append(firstDate + timedelta(days=datesIterator))
+
+for filterProject in ticketProjects:
+    projectsStatistics[filterProject]={}
+    for filterDateIterator in (filterDates):
+        # jsonProjectsStats[str(filterDateIterator)][filterProject]={}
+        previousDay=filterDateIterator - timedelta(days=1)
+        projectsStatistics[filterProject][str(filterDateIterator)] = getFiltersResults.generateStatistics(filterProject, ticketStatuses,
+                                                                                 ticketPriority,
+                                                                                 filterDateIterator.strftime('%Y/%m/%d'),
+                                                                                 previousDay.strftime('%Y/%m/%d'))
+
+
+
+jsonResults=json.dumps(projectsStatistics, sort_keys=False, indent=4)
+fileName = str(todayDate) + "_" + currentTime + "_results.json"
+dirPath = defaultLogsLocation
+resultFile = open(os.path.join(dirPath, fileName), "x")
+resultFile.write(jsonResults)
+resultFile.close()
+
+
+sendEmail.sendEmail(jsonResults)
